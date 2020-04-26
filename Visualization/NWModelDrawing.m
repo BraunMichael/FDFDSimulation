179ea01d84a3bf77b6f7581e5a5699fa1c3dbb9a
braun <mbraun848@gmail.com>
2020-04-26 14:54:55 -0700

    Update NWModelDrawing with rotation

--- Visualization/NWModelDrawing.m
+++ Visualization/NWModelDrawing.m
@@ -1,4 +1,4 @@
-function NWModelDrawing(substrateThickness, NWLength, NWDiameter, NWspacing_x, NWspacing_z, Laser_angle)
+function NWModelDrawing_rotated(substrateThickness, NWLength, NWDiameter, NWspacing_x, NWspacing_z, Laser_angle, xoffset, axishandle)
 
 NWRadius = NWDiameter/2;
 [xNW,yNW,zNW] = cylinder(NWRadius, 100);
@@ -9,17 +9,17 @@ r = NWRadius;
 germanium = [0.5 0.5 0.5]; %[0.400000000 0.560784314 0.560784314]; %CPK coloring, otherwise [0.5 0.5 0.5]
 gold = [1 215/255 0];
 
-h = surf(r.*xTip,r.*yTip,r.*zTip + NWLength + substrateThickness);
+h = surf(axishandle, r.*xTip + xoffset,r.*zTip + NWLength + substrateThickness,r.*yTip);
 set(h, 'edgecolor', 'none', 'facecolor', gold, 'FaceLighting','gouraud');
 
-t = surf(xNW,yNW,zNW);
+t = surf(axishandle, xNW + xoffset,zNW,yNW);
 set(t, 'edgecolor', 'none', 'facecolor', germanium, 'FaceLighting', 'gouraud');
 
-b = fill3(0.5*NWspacing_x*[-1 -1 1 1],0.5*NWspacing_z*[-1 1 1 -1],substrateThickness*[0 0 0 0],[0.5 0.5 0.5], ...  % Y faces
- 0.5*NWspacing_x*[-1 -1 1 1],0.5*NWspacing_z*[-1 1 1 -1],substrateThickness*[1 1 1 1],germanium, ...  % Y faces
- 0.5*NWspacing_x*[-1 -1 -1 -1],0.5*NWspacing_z*[-1 1 1 -1],substrateThickness*[0 0 1 1],germanium, ... % Z faces
- 0.5*NWspacing_x*[1 1 1 1],0.5*NWspacing_z*[-1 1 1 -1],substrateThickness*[0 0 1 1],germanium, ...  % Z faces
- 0.5*NWspacing_x*[-1 -1 1 1],0.5*NWspacing_z*[-1 -1 -1 -1],substrateThickness*[0 1 1 0],germanium,... % X faces
- 0.5*NWspacing_x*[-1 -1 1 1],0.5*NWspacing_z*[1 1 1 1],substrateThickness*[0 1 1 0],germanium) ;  % X faces
+b = fill3(axishandle, 0.5*NWspacing_x*[-1 -1 1 1] + xoffset,substrateThickness*[0 1 1 0],0.5*NWspacing_z*[-1 -1 -1 -1],[0.5 0.5 0.5], ...  % Y faces
+ 0.5*NWspacing_x*[-1 -1 1 1] + xoffset,substrateThickness*[0 1 1 0],0.5*NWspacing_z*[1 1 1 1],germanium, ...  % Y faces
+ 0.5*NWspacing_x*[-1 -1 -1 -1] + xoffset,substrateThickness*[0 1 1 0],0.5*NWspacing_z*[-1 -1 1 1],germanium, ... % Z faces
+ 0.5*NWspacing_x*[1 1 1 1] + xoffset,substrateThickness*[0 1 1 0],0.5*NWspacing_z*[-1 -1 1 1],germanium, ...  % Z faces
+ 0.5*NWspacing_x*[-1 -1 1 1] + xoffset,substrateThickness*[0 0 0 0],0.5*NWspacing_z*[-1 1 1 -1],germanium,... % X faces
+ 0.5*NWspacing_x*[-1 -1 1 1] + xoffset,substrateThickness*[1 1 1 1],0.5*NWspacing_z*[-1 1 1 -1],germanium) ;  % X faces
 set(b, 'FaceAlpha', 1, 'FaceLighting', 'gouraud') ;
 
