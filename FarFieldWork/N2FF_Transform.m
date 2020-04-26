96289c54c823dee7f9172c85ee95bb4dcce7fa87
braun <mbraun848@gmail.com>
2020-04-26 15:16:43 -0700

    Update N2FF_Transform to V2

--- FarFieldWork/N2FF_Transform.m
+++ FarFieldWork/N2FF_Transform.m
@@ -1,14 +1,17 @@
 %% Near field to far field transformation
 close all; clear all; clc;
 tic
-remote = true;
+remote = false;
 limited_range = true; %limits angular range, increases point resolution
 
 if limited_range
     center_theta = pi/6;
     center_phi = pi;
-    anglerange_deg = 6; %Sets range of angles used if limited_range is true
+    anglerange_deg = 6.6; %Sets range of angles used if limited_range is true, corresponds to actual angles
     points_per_deg = 10; %Sets number of points per degree
+else
+    n_theta = 360;
+    n_phi = 360;
 end
 
 if remote
@@ -28,12 +31,8 @@ if remote
     end
 else %at home
     folderpath = uigetdir();
+    cd(folderpath)
 end
-%Set this folder as current directory
-cd(folderpath)
-
-
-
 
 
 %% Setup box
@@ -47,16 +46,15 @@ n_strates=[1, 5.483850];
 z_strates=[0]; %Might want to read in substrate thickness and enter as z_strates
 grid_spacing = 20; %point spacing in nm
 distance_above = 20; %nm above source to use in integration
-domainsize = 10; %in ?m, x-y dimensions of box to integrate
+domainsize = 10; %in um, x-y dimensions of box to integrate
 
-n_theta = 360;
-n_phi = 360;
+
 %% Field Prep
 
 % Get a list of all files in the folder with the desired file name pattern.
 filePattern = fullfile(folderpath, '*.E.h5');
 theFiles = dir(filePattern);
-[tmp, ind] = sort({theFiles.date});
+[tmp, ind] = natsortfiles({theFiles.name});
 theFiles = theFiles(ind);
 for k = 1 : length(theFiles)
     baseFileName = theFiles(k).name;
@@ -66,8 +64,7 @@ for k = 1 : length(theFiles)
     load(Simfilename_only)
     fprintf('Working on %s\n', Simfilename_only);
     [E, H] = read_output(Simfilename_only);
-    save('saved_field_matrix.mat', 'E', 'H', 'obj_array', 'src_array')
-    
+    save('saved_field_matrix.mat', 'E', 'H', 'obj_array', 'src_array', '-v7.3')
     primy_temp = E{Axis.x}.grid3d.lall{2,1};
     min_y = min(min(primy_temp)); max_y = max(max(primy_temp));
     
@@ -79,8 +76,8 @@ for k = 1 : length(theFiles)
     L=[domainsize * 1e-6, domainsize * 1e-6, abs(height - min_y) * 1e-9]; %in meters
     N_gauss = round((L .* 1e9)./ grid_spacing) + 1;
     %% Calculate init
-    init = retop(@FieldInterpolation_onbox_v2,k0,n_strates,z_strates,Centre,L,N_gauss,struct('option_cal_champ',option_cal_champ,'option_i',option_i));
-    
+    init = retop(@FieldInterpolation_onbox_v3,k0,n_strates,z_strates,Centre,L,N_gauss,struct('option_cal_champ',option_cal_champ,'option_i',option_i));
+    return
     %% Compute the plane wave decomposition in free space
     if limited_range
         theta = linspace(center_theta - (anglerange_deg*0.5*pi/180), center_theta +(anglerange_deg*0.5*pi/180), points_per_deg*anglerange_deg + 1);
@@ -99,8 +96,13 @@ for k = 1 : length(theFiles)
     vh=k0*n_strates(1)*v;
     angles_h=retop(init,uh,vh,1,struct('test',1));% calculate top angles
     Fh=reshape(angles_h.F,size(uh));
+    u_int = u(:,1)';
+    v_int = v(1,:);
+    integ = trapz(v_int, trapz(u_int,Fh, 1));
+    length = obj_array(2).shape.L(2);
+    
     savefilename = strcat(Simfilename_only, '_farfield.mat');
-    save(savefilename, 'angles_h', 'init', 'Fh', 'uh', 'vh', 'u', 'v', 'w')
+    save(savefilename, 'angles_h', 'init', 'Fh', 'uh', 'vh', 'u', 'v', 'w', 'integ', 'length')
     
     if remote
     else
@@ -113,7 +115,7 @@ for k = 1 : length(theFiles)
         title('radiation diagram');
         %ret_normalise_dop(1.e13);
     end
-        
+    
 end
 toc
 %
