e05aee0efff677b5ccf140902b5b5003bf0d9531
braun <mbraun848@gmail.com>
2020-04-26 13:46:33 -0700

    Update FieldIntegration_3D to V4

--- FieldIntegration_3D.m
+++ FieldIntegration_3D.m
@@ -1,6 +1,9 @@
 close all;
 clear all;
 clc;
+
+max_NWLength = 2000;
+
 slice_axis = Axis.y;
 slice_location = 50; %if slice_axis = Axis.y, this is nm above source (negative if you want to slice below source height
 even_interpolation = true; %Always want this to be true for integration
@@ -23,12 +26,15 @@ k_v = 0;%Shouldn't need to change this, horizontal (+z) wave vector of incident 
 %Set this folder as current directory
 mainfolderpath=uigetdir('','Select folder with files');
 cd(mainfolderpath)
+
+tic
+
 D = dir;
 D = D(~ismember({D.name}, {'.', '..'}));
 for m = 1:numel(D)
     if D(m).isdir
-        [backgroundFile, numberrepeats] = backgroundFieldLogicIntegration(D(m).name);
-
+        [backgroundFile, numberrepeats] = backgroundFieldLogicIntegration(D(m).name, max_NWLength);
+        
         
         load(backgroundFile)
         Background_Fields = {E_background, H_background};
@@ -45,12 +51,12 @@ for m = 1:numel(D)
         theFiles = dir(filePattern);
         [tmp, ind] = natsortfiles({theFiles.name});
         theFiles = theFiles(ind);
-        for k = 1 : length(theFiles)
+        parfor k = 1 : length(theFiles)
             solutionfilename = theFiles(k).name;
             [~, intermediate, ~]=fileparts(solutionfilename);
             [~, filenamebase, ~]=fileparts(intermediate);
             fprintf('Working on %s\n', filenamebase);
-            load(filenamebase)
+            savedArrays = load(filenamebase);
             [E, H] = read_output(filenamebase);
             Fields = {E, H};
             Compiled_fields = cell(2,1);
@@ -59,27 +65,49 @@ for m = 1:numel(D)
             
             
             
-            if strfind(filenamebase, 'L_0_')
+            if ~isempty(strfind(filenamebase, 'L_0_')) || ~isempty(strfind(filenamebase, '_L0_'))
                 NWlength = 0;
             else
-                for i=1:max(size(obj_array))
-                    if ~strcmp(obj_array(i).material.name, 'vacuum')
-                        if isa(obj_array(i).shape, 'CircularCylinder')
-                            NWlength = obj_array(i).shape.L(2);
-                            NWDiameter = obj_array(i).shape.L(1);
+                for i=1:max(size(savedArrays.obj_array))
+                    if ~strcmp(savedArrays.obj_array(i).material.name, 'vacuum')
+                        if isa(savedArrays.obj_array(i).shape, 'CircularCylinder')
+                            NWlength = savedArrays.obj_array(i).shape.L(2);
+                            NWDiameter = savedArrays.obj_array(i).shape.L(1);
                             NWRadius = NWDiameter/2;
                         end
                     end
                 end
             end
-            length_series(k) = NWlength;
+            
+            try
+                length_series(k) = NWlength;
+                diameter_series(k) = NWDiameter;
+                
+            catch
+                for i=1:max(size(savedArrays.obj_array))
+                    if ~strcmp(savedArrays.obj_array(i).material.name, 'vacuum')
+                        if isa(savedArrays.obj_array(i).shape, 'Box')
+                            NWbottom = savedArrays.obj_array(i).shape.bound(2,2)
+                        end
+                        if isa(savedArrays.obj_array(i).shape, 'Hemisphere')
+                            NWtop = savedArrays.obj_array(i).shape.bound(2,1);
+                            NWDiameter = savedArrays.obj_array(i).shape.L(1);
+                            NWRadius = NWDiameter/2;
+                        end
+                    end
+                end
+                NWlength = NWtop - NWbottom;
+                length_series(k) = NWlength;
+                diameter_series(k) = NWDiameter;
+            end
+            
             % All where they should be...
             % domainHeight(k) = obj_array(1).shape.L(2);
             % domainHeightCenter(k) = obj_array(1).shape.cb_center(2);
             % sourceHeight(k) = src_array.intercept;
             
             
-            [E_compiled, H_compiled, outu, outv, domainwidth, domaindepth, k_u, k_v] = fieldSliceBackgroundSubtract(Fields, Background_Fields, k_u, k_v, source_height_background, slice_axis, slice_location, even_interpolation, interpolation_gridsize, calculate, src_array);
+            [E_compiled, H_compiled, outu, outv, domainwidth, domaindepth, k_u_read, k_v_read] = fieldSliceBackgroundSubtract(Fields, Background_Fields, k_u, k_v, source_height_background, slice_axis, slice_location, even_interpolation, interpolation_gridsize, calculate, savedArrays.src_array);
             
             
             E2 = E_compiled(:, :, 1).*conj(E_compiled(:, :, 1)) + E_compiled(:, :, 2).*conj(E_compiled(:, :, 2)) + E_compiled(:, :, 3).*conj(E_compiled(:, :, 3));
@@ -102,35 +130,61 @@ for m = 1:numel(D)
             %
             
         end
+        toc
+        
+        solutionfilename = theFiles(1).name;
+        [~, intermediate, ~]=fileparts(solutionfilename);
+        [~, filenamebase, ~]=fileparts(intermediate);
+        load(filenamebase) % This just loads in the obj and src arrays to save to the new mat file below
+        domainwidth = obj_array(1).shape.L(1);
+        domaindepth = obj_array(1).shape.L(3);
         
         if show_figures
-            fieldExtensionIntegrationAndPlot(E_compiled(:,:,3), outu, outv, numberrepeats, k_u, 0, domainwidth, domaindepth, slice_axis, ygrid, symmetric_color, show_figures);
+            fieldExtensionIntegrationAndPlot(E_compiled(:,:,3), outu, outv, numberrepeats, k_u_read, 0, domainwidth, domaindepth, slice_axis, ygrid, symmetric_color, show_figures);
             fieldExtensionIntegrationAndPlot(E2, outu, outv, numberrepeats, 0, 0, domainwidth, domaindepth, slice_axis, ygrid, false, show_figures);
         end
         [length_series, length_indecies] = sort(length_series);
-        integ_series = integ_series(length_indecies);
+        [diameter_series, diameter_indecies] = sort(diameter_series);
+        isConstantLength = any((length_series-length_series(1)),2);
+        if isConstantLength
+            integ_series = integ_series(length_indecies);
+            plot_series = length_series;
+            xPlotLabel = '$$L$$ (nm)';
+        else
+            integ_series = integ_series(diameter_indecies);
+            plot_series = diameter_series;
+            xPlotLabel = '$$D$$ (nm)';
+        end
+        
         
         
         disp('Done')
         figure();
-        plot(length_series, integ_series/integ_series(1), 'ko-')
+        plot(plot_series, integ_series/integ_series(1), 'ko-')
         title(D(m).name,'Interpreter','latex')
         %title('s=300 vs 400 - Background Emission Subtracted','Interpreter','latex')
-        xlabel('$$L$$ (nm)','interpreter','latex')
+        xlabel(xPlotLabel,'interpreter','latex')
         ylabel('Intensity (arb units)','interpreter','latex')
         axis square
         %legend('7 wires/um2', '11 wires/um2')
         drawnow
         
         %Export
-        save(strcat(D(m).name, 'savefile.mat'), 'length_series', 'integ_series', 'src_array', 'obj_array', 'domainwidth', 'domaindepth', 'numberrepeats')
+        
+        save(strcat(D(m).name, 'savefile.mat'), 'length_series', 'diameter_series', 'integ_series', 'src_array', 'obj_array', 'domainwidth', 'domaindepth', 'numberrepeats')
         integFileSave = strcat(D(m).name, '_origin.txt');
         fid=fopen(integFileSave,'wt');
-        fprintf(fid, [ 'Length (nm)' '\t' 'Intensity (arb units)' '\t' 'Normalized Intensity' '\r\n']);
-        fprintf(fid, '%f\t%f\t%f\r\n', [length_series' integ_series' (integ_series/integ_series(1))']');
+        if isConstantLength
+            fprintf(fid, [ 'Length (nm)' '\t' 'Intensity (arb units)' '\t' 'Normalized Intensity' '\r\n']);
+            fprintf(fid, '%f\t%f\t%f\r\n', [length_series' integ_series' (integ_series/integ_series(1))']');
+        else
+            fprintf(fid, [ 'Diameter (nm)' '\t' 'Intensity (arb units)' '\t' 'Normalized Intensity' '\r\n']);
+            fprintf(fid, '%f\t%f\t%f\r\n', [diameter_series' integ_series' (integ_series/integ_series(1))']');
+        end
         fclose(fid);true;
         
         cd ..
     end
 end
+
 
